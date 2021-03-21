486c85fd0f991f76cab6e0803ec1b0bc18ae7340
Chen Sihan <schen@lcs.on.ca>
2021-03-20 23:51:04 -0400

    Delete extra views: HistoryView and CreditView and ReferenceView

--- SchoolLifeGuideNew/View/FAQs/AssessmentView.swift
+++ SchoolLifeGuideNew/View/FAQs/AssessmentView.swift
@@ -45,36 +45,6 @@ struct AssessmentView: View {
                         Section {
                             
                             TextField("Description", text: $viewModel.description)
-                            
-                            //                            Picker("Offence Type", selection: $viewModel.offenceType) {
-                            //                                Text(OffenceType.InappropriateDress.rawValue).tag(OffenceType.InappropriateDress)
-                            //                                Text(OffenceType.LateForClass.rawValue).tag(OffenceType.LateForClass)
-                            //                                Text(OffenceType.UnexcusedAbsencesForClass.rawValue).tag(OffenceType.UnexcusedAbsencesForClass)
-                            //                                Text(OffenceType.UnexcusedAbsencesForCoCurriculars.rawValue).tag(OffenceType.UnexcusedAbsencesForCoCurriculars)
-                            //                                Text(OffenceType.ViolationOfRUA.rawValue).tag(OffenceType.ViolationOfRUA)
-                            //                                Text(OffenceType.ViolationOfFirePolicies.rawValue).tag(OffenceType.ViolationOfFirePolicies)
-                            //                                Text(OffenceType.DormViolations.rawValue).tag(OffenceType.DormViolations)
-                            //                                Text(OffenceType.Trepassing.rawValue).tag(OffenceType.Trepassing)
-                            //                                Text(OffenceType.Theft.rawValue).tag(OffenceType.Theft)
-                            //                                Text(OffenceType.HaveStolenProperty.rawValue).tag(OffenceType.HaveStolenProperty)
-                            //                                Text(OffenceType.AWOL.rawValue).tag(OffenceType.AWOL)
-                            //                                Text(OffenceType.DamagedProperty.rawValue).tag(OffenceType.DamagedProperty)
-                            //                                Text(OffenceType.OnCampusSmoking.rawValue).tag(OffenceType.OnCampusSmoking)
-                            //                                Text(OffenceType.OffCampusSmokingIllegal.rawValue).tag(OffenceType.OffCampusSmokingIllegal)
-                            //                                Text(OffenceType.OffCampusSmokingLegal.rawValue).tag(OffenceType.OffCampusSmokingLegal)
-                            //                                Text(OffenceType.TraffickingENDS.rawValue).tag(OffenceType.TraffickingENDS)
-                            //                                Text(OffenceType.SmokingNearBuilding.rawValue).tag(OffenceType.SmokingNearBuilding)
-                            //                                Text(OffenceType.OffCampusDrinking.rawValue).tag(OffenceType.OffCampusDrinking)
-                            //                                Text(OffenceType.OnCampusDrinking.rawValue).tag(OffenceType.OnCampusDrinking)
-                            //                                Text(OffenceType.TraffickingAlcohol.rawValue).tag(OffenceType.TraffickingAlcohol)
-                            //                                Text(OffenceType.IllegalMedication.rawValue).tag(OffenceType.IllegalMedication)
-                            //                                Text(OffenceType.OnCampusDrugUse.rawValue).tag(OffenceType.OnCampusDrugUse)
-                            //                                Text(OffenceType.OffCampusDrugUse.rawValue).tag(OffenceType.OffCampusDrugUse)
-                            //                                Text(OffenceType.TraffickingCannabis.rawValue).tag(OffenceType.TraffickingCannabis)
-                            //                                Text(OffenceType.OtherIllegalDrugs.rawValue).tag(OffenceType.OtherIllegalDrugs)
-                            //                                Text(OffenceType.PersistentFailExpectations.rawValue).tag(OffenceType.PersistentFailExpectations)
-                            
-                            //                            }.pickerStyle(WheelPickerStyle())
                             WheelPickerView(viewModel: viewModel)
                             Picker("Violation Times", selection: $viewModel.violationTimes) {
                                 Text(TimesOfViolation.First.rawValue).tag(TimesOfViolation.First)
@@ -109,11 +79,7 @@ struct AssessmentView: View {
                             
                         }
                         
-                        Section {
-                            NavigationLink("Reference Level List" , destination: ReferenceView() )
-                            
-                            NavigationLink("Credit", destination: CreditView())
-                        }
+                        
                         
                         
                         
@@ -144,63 +110,46 @@ struct AssessmentView: View {
                     Text("Cancel").foregroundColor(.white).onTapGesture {
                         saveTask()
                     }
-                    //                    Button("Cancel") {
-                    //                        saveTask()
-                    //                    }.foregroundColor(.white)
                     
-                }
-                
-                ToolbarItem(placement: .primaryAction) {
-                    
-                    
-                    
-                    
-                    
-                    HStack {
-                        
-                        
-                        Image(systemName: "clock.fill")
-                        Text("History")
-                    }.foregroundColor(.white)
-                    .onTapGesture {
-                        showingHistoryView = true
-                    }
                     
                 }
                 
                 
-            }.sheet(isPresented: $showingHistoryView) {
-                HistoryView(store: store, showing: $showingHistoryView)
+                
+            }
+            
+            
+            
+            
+            
+        }
+        
+    }
+        func saveTask() {
+            
+            // Add the task to the list of tasks
+            store.offences.append(Offence(areaOfViolation: viewModel.description, timesOfViolation: viewModel.violationTimes, severity: viewModel.severity, levelOfCompliance: viewModel.complianceLevel))
+            
+            // Dismiss this view
+            showing = false
+            
+        }
+        
+        struct ColorStyle: ButtonStyle {
+            func makeBody(configuration: Self.Configuration) -> some View {
+                configuration.label
+                    .padding(12)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
             }
         }
         
-        
-        
-        
-    }
-    func saveTask() {
-        
-        // Add the task to the list of tasks
-        store.offences.append(Offence(areaOfViolation: viewModel.description, timesOfViolation: viewModel.violationTimes, severity: viewModel.severity, levelOfCompliance: viewModel.complianceLevel))
-        
-        // Dismiss this view
-        showing = false
-        
-    }
-    
-    struct ColorStyle: ButtonStyle {
-        func makeBody(configuration: Self.Configuration) -> some View {
-            configuration.label
-                .padding(12)
-                .foregroundColor(.white)
-                .cornerRadius(8)
+        struct AssessmentView_Previews: PreviewProvider {
+            static var previews: some View {
+                AssessmentView(showing: .constant(true), store: testStore)
+            }
         }
     }
-    
-    struct AssessmentView_Previews: PreviewProvider {
-        static var previews: some View {
-            AssessmentView(showing: .constant(true), store: testStore)
-        }
-    }
-}
+
+
 var accountabilityText = ""
